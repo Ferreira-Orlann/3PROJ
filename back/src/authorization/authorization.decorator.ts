993a8a783cb0d4ceae914797b436afb10ce5ba4c
Ferreira-Orlann <118252904+Ferreira-Orlann@users.noreply.github.com>
2025-02-22 12:57:46 +0100

    Update

--- back/src/authorization/authorization.decorator.ts
+++ back/src/authorization/authorization.decorator.ts
@@ -1,5 +1,6 @@
 import { ExecutionContext, SetMetadata } from "@nestjs/common";
 import { UUID } from "crypto";
+import { Request } from "express";
 
 export const PERMISSIONS_METADATA = "__SUP_PHONE_PERMISSIONS_METADATA"
 
@@ -14,3 +15,14 @@ export type UsePermission = (context: ExecutionContext) => Permission
 export const Authorize = (use: UsePermission) => {
     return SetMetadata(PERMISSIONS_METADATA, use);
 };
+
+export function HttpWorkspaceResource(action: string, resourceParam: string = "", workspaceParam: string = ""): UsePermission {
+    return (context: ExecutionContext) => {
+        const request: Request = context.switchToHttp().getRequest() as Request
+        return {
+            domain: request.params[workspaceParam],
+            resource: request.params[resourceParam],
+            action: action
+        } as Permission
+    }
+}
--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -5,21 +5,14 @@ import { UUID } from "crypto";
 import { AuthActionVerb, AuthZGuard, AuthZService, UsePermissions } from "nest-authz";
 import { HttpAuthGuard, IAuthRequest } from "src/authentication/http.authentication.guard";
 import { AuthorizationGuard } from "src/authorization/authorization.guard";
-import { Authorize, Permission } from "src/authorization/authorization.decorator";
+import { Authorize, HttpWorkspaceResource, Permission } from "src/authorization/authorization.decorator";
 
 @Controller("users")
 export class UsersController {
     constructor(private readonly usersService: UsersService, private readonly authzService: AuthZService) {}
 
     @Get()
-    @UseGuards(HttpAuthGuard, AuthorizationGuard)
-    @Authorize((context) => {
-        return {
-            domain: "1f790545-d0a6-4bb6-97e7-80adf82913be",
-            resource: "21a78b2c-a5de-49a0-bfa5-b1a73eab5013",
-            action: "CREATE"
-        }
-    })
+    @UseGuards(HttpAuthGuard)
     async get(@Request() req: IAuthRequest) {
         console.log(req.user)
         const en = await this.authzService.enforce(req.user?.uuid, "test", "message", "CREATE")
