8c8619a13bf259408b0ac2603eef2c137e16ed18
Ferreira-Orlann <118252904+Ferreira-Orlann@users.noreply.github.com>
2025-02-25 11:37:55 +0100

    Add Multer types and controler

--- back/src/file/files.controller.ts
+++ back/src/file/files.controller.ts
@@ -1,6 +1,10 @@
-import { Controller } from "@nestjs/common";
+import { Controller, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller("files/:fileUuid")
 export class FilesController {
-
+    @UseInterceptors(FileInterceptor('file'))
+    upload(@UploadedFile() file: Express.Multer.File) {
+        
+    }
 }
