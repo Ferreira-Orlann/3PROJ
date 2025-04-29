c3d5715057cc5cc419339201a790c433a25fb92b
Ferreira-Orlann <118252904+Ferreira-Orlann@users.noreply.github.com>
2025-04-29 15:56:15 +0200

    Websocket Bug Resolution

--- src/back/websockets/websocket.module.ts
+++ src/back/websockets/websocket.module.ts
@@ -1,9 +1,6 @@
-import { Module, OnApplicationBootstrap } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { WebSocketPool } from "./websocket_pool.gateway";
-import { WebSocketAuth } from "./forwarder.gateway";
 import { MessagesListener } from "./listeners/messages";
-import { Test } from "./test";
-import { SupGatewayService } from "./supgateway.service";
 import { MessagesModule } from "../messages/messages.module";
 import { AuthModule } from "../authentication/authentication.module";
 import { ReactionsModule } from "../reactions/reactions.module";
@@ -13,19 +10,10 @@ import { MessageHandler } from "./handlers/message.handler";
     imports: [AuthModule, MessagesModule, ReactionsModule],
     controllers: [],
     providers: [
-        SupGatewayService,
         WebSocketPool,
-        WebSocketAuth,
         MessagesListener,
         ReactionsListener,
         MessageHandler,
-        Test,
     ],
 })
-export class WebSocketModule implements OnApplicationBootstrap {
-    constructor(private readonly supGatewayService: SupGatewayService) {}
-
-    onApplicationBootstrap() {
-        this.supGatewayService.explore();
-    }
-}
+export class WebSocketModule {}
