67bd64020a3a1115cca8d198a6280181270b63d6
Ferreira-Orlann <118252904+Ferreira-Orlann@users.noreply.github.com>
2025-05-09 01:32:29 +0200

    Workspaces Service

--- src/front/services/workspaces.service.ts
+++ src/front/services/workspaces.service.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
     import authService from "./auth.service";
 import { Workspace } from "../types/workspace";
+import { UUID } from "crypto";
 
 class WorkspacesService {
     public async create(name: string, description: string, is_public: boolean): Promise<Workspace> {
@@ -16,6 +17,36 @@ class WorkspacesService {
         });
         return new Promise((resolve, reject) => {
             if (response.status != 201) {
+                reject(response.data)
+            }
+            resolve(response.data)
+        })
+    }
+
+    public async update(workspace: Partial<Workspace>) {
+        const response = await axios.put<Workspace>("http://localhost:3000/workspaces", JSON.stringify(workspace), {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${authService.getSession().token}`,
+            }
+        });
+        return new Promise((resolve, reject) => {
+            if (response.status != 204) {
+                reject(response.data)
+            }
+            resolve(response.data)
+        })
+    }
+
+    public async getByUUID(uuid: UUID): Promise<Workspace> {
+        const response = await axios.get<Workspace>(`http://localhost:3000/workspaces/${uuid}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${authService.getSession().token}`,
+            }
+        });
+        return new Promise((resolve, reject) => {
+            if (response.status != 200) {
                 reject(response.data)
             }
             resolve(response.data)
